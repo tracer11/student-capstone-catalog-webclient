377d774c99150d16b5d813683294992458967428
tracer11 <karolgurba22@gmail.com>
2017-07-03 20:55:40 -0500

    updated controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,21 @@
 class StudentsController < ApplicationController
  
   def index
-    students_hash = Student.all
-
     @students = []
-
-    students_hash.each do |student_hash|
+    Unirest.get(
+                "#{ENV["API_HOST"]}/api/v1/students.json",
+                headers:{
+                        "Accept" => "application/json",
+                        }
+                ).body.each do |student_hash|
       @students << Student.new(student_hash)
     end
   end
 
   def show
-    @student = Student.find(params[:id])
+    @student = Unirest.get("#{ENV["API_HOST"]}/api/v1/students/#{id}.json",
+                          headers:{
+                                  "Accept" => "application/json"
+                            }).body
   end
 end
